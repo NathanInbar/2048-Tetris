2c6e9585bf5943f3536087b44994e286263eb6ed
nathaninbar <nathaninbar@uvic.ca>
2025-03-14 20:24:07 -0700

    refactored, needs bug fixes

---
+++ Scripts/Globals.ts
@@ -0,0 +1,8 @@
+export class Globals {
+    static readonly canvasWidth:number = 420;
+    static readonly canvasHeight:number = 420;
+    static readonly squareSize:number = 60;
+    static readonly nRows:number = 7;
+    static readonly nCols:number = 7;
+
+}
--- Scripts/TileShiftResult.ts
+++
@@ -1,6 +0,0 @@
-export enum TileShiftResult {
-    FAIL_OUTOFBOUNDS = 0,
-    FAIL_OCCUPIED = 1,
-    SUCCESS_SHIFTED = 2,
-    SUCCESS_MERGED = 3 
-}
